eab6285aa3ae530b13f95d692bc0fab165e9055a
parksuk1991 <148017439+parksuk1991@users.noreply.github.com>
2025-07-22 13:20:03 +0900

    Update app_repository.py

--- app_repository.py
+++ app_repository.py
@@ -434,7 +434,7 @@ def create_performance_charts(portfolio_returns, benchmark_returns, benchmark_na
     return fig_yearly, fig_monthly
 
 def main():
-    col_title, col_img_credit = st.columns([8, 1])
+    col_title, col_img_credit = st.columns([6, 1])
     with col_title:
         st.title("📈 Portfolio Backtesting App")
         #st.markdown("##### 만든이: 박석")
@@ -466,13 +466,13 @@ def main():
 
 
         st.markdown(
-        "<div style='margin-top: -1px; text-align:right;'>"
+        "<div style='margin-top: -1px; text-align:center;'>"
         "<span style='font-size:0.9rem; color:#888;'>Made by parksuk1991</span>"
         "</div>",
         unsafe_allow_html=True
         )
         st.markdown(
-            '<div style="text-align: center; margin-bottom: 9px;">'
+            '<div style="text-align: right; margin-bottom: 9px;">'
             'Data 출처: <a href="https://finance.yahoo.com/" target="_blank">Yahoo Finance</a>'
             '</div>',
             unsafe_allow_html=True
